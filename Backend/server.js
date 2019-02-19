0a22f4b8569a311badfcb4a93d6d10dcdabdba66
Paul Tobin <tobster111@gmail.com>
2019-02-19 18:20:49 +0000

    Added cors to the server

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+
+const upload = require('./upload');
 
 const server = express();
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+};
+
+server.use(cors(corsOptions));
+
+server.post('/upload', upload);
 
 server.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
