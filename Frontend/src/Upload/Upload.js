431992f47524d138ccd082fcd064279cb4931097
Paul Tobin <tobster111@gmail.com>
2019-02-20 09:47:30 +0000

    Added a basic upload layout

---
+++ Frontend/src/Upload/Upload.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Upload.css";
+
+class Upload extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return <p>Upload</p>
+  }
+}
+
+export default Upload;
