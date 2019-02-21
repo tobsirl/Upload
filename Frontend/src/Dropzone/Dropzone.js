98dffb8ed86bf95cae68993eac69409e02527c3c
Paul Tobin <tobster111@gmail.com>
2019-02-21 11:01:18 +0000

    Added a dropzone component

---
+++ Frontend/src/Dropzone/Dropzone.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Dropzone.css';
+
+class Dropzone extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>Dropzone</p>;
+  }
+}
