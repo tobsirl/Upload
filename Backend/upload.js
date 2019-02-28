571623eb9c94c2e5f6974c3991a5aba63ceca598
Paul Tobin <tobster111@gmail.com>
2019-02-28 11:07:04 +0000

    Added mongoose to make the db connection, file upload is working locally

--- Backend/upload.js
+++ Backend/upload.js
@@ -3,11 +3,15 @@ const IncomingForm = require('formidable').IncomingForm;
 module.exports = function upload(req, res) {
   const form = new IncomingForm();
 
-  form.on('file', (field, file) => {
+  form.on('file', (name, file) => {
     // Do something with the file
     // e.g. save it to the database
     // you can access it using file.path
-    console.log(file.path, 'File Upload');
+    // Desructing
+
+    file.path = __dirname + '/upload/' + file.name;
+
+    console.log('File Upload', name, file);
   });
 
   form.on('end', () => {
