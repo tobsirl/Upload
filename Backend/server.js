b1a26b849634fe3cdb128abdc78ddbd9cc33a49d
Paul Tobin <tobster111@gmail.com>
2019-02-28 18:30:22 +0000

    Installed dotenv to handle environmental variables, db connection is working

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const upload = require('./upload');
 
 const server = express();
@@ -15,7 +17,16 @@ server.use(cors(corsOptions));
 
 server.post('/upload', upload);
 
-const PORT = 8000;
+const { PORT, MONGO_URI } = process.env;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true }).then(
+  () => {
+    console.log(`Connected to database`);
+  },
+  err => {
+    console.log(err);
+  }
+);
 
 server.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
